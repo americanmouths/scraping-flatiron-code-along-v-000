0277bf5f9600ad97c15a1c9e90240feda526aa32
Kayla Hansen <shiverr@gmail.com>
2017-10-25 00:47:34 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,17 @@
 class Courserspec
   attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+    
+  def self.all?
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
