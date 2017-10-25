7296aa4941b6ed9ffcc76039898a017340e0b154
Kayla Hansen <shiverr@gmail.com>
2017-10-25 01:03:38 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,10 +19,9 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
-  end
 
-  def get_courses
+    
+  end
 
 
 end
